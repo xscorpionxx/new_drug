580b243aa82b9e669a566ba845ae49c02b20961e
xscorpionxx <almuazenknight1969@gmail.com>
2023-02-10 13:37:18 -0800

    'first_'

--- main.py
+++ main.py
@@ -9,19 +9,17 @@ from selenium.webdriver.support import expected_conditions as EC
 import os
 app = FastAPI()
  
+
 @app.get("/")
-def root():
-    return {"message": "Hello__________"}
-@app.get("/init")
 def root_():
     global driver
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
-    driver.get('https://www.drugs.com/drug_interactions.html')
+    # chrome_options = webdriver.ChromeOptions()
+    # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    # chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--disable-dev-shm-usage")
+    # chrome_options.add_argument("--no-sandbox")
+    # driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+    # driver.get('https://www.drugs.com/drug_interactions.html')
     return {"message": "Hello__________"}
 
 # class drugs(BaseModel):
